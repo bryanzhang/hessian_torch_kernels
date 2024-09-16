35b5f35be02600f9a31328e6fd171dfbe71174bb
Bryan Zhang <johnhall@126.com>
2024-09-16 15:43:17 +0800

    Update hessianorpinv_matmul_vector.cpp

--- hessianorpinv_matmul_vector.cpp
+++ hessianorpinv_matmul_vector.cpp
@@ -3,13 +3,14 @@
 #include <immintrin.h>
 
 using namespace std;
+namespace py = pybind11;
 
 // 海赛矩阵或其逆阵乘以一个列向量。
 // 单层网络中，每个weight的海塞矩阵比较稀疏，但用稀疏矩阵存储仍超出内存使用限制，只有对角上的block有值，且这些小方阵值完全相同，其他位置均为0.
 // 其伪逆阵也同样如此。
+// 必须确保所有的tensor都是连续的
 torch::Tensor hessianorpinv_matmul_vector_naive(const torch::Tensor& subblock, const torch::Tensor& vec, int num_block) {
     assert(subblock.dim() == 2 && vec.dim() == 2 && subblock.size(1) * num_block == vec.size(0) && vec.size(1) == 1);
-    cout << "-O3" << endl;
     auto ret = torch::zeros_like(vec, torch::kFloat32);
     int64_t num_substride = subblock.size(0);
     for (int64_t i = 0; i < num_block; ++i) {
@@ -24,15 +25,16 @@ torch::Tensor hessianorpinv_matmul_vector_naive(const torch::Tensor& subblock, c
     return ret;
 }
 
-// avx2 simd优化
+// avx simd优化
+// 必须确保所有的tensor都是连续的
 torch::Tensor hessianorpinv_matmul_vector_avx2(const torch::Tensor& subblock, const torch::Tensor& vec, int num_block) {
     assert(subblock.dim() == 2 && vec.dim() == 2 && subblock.size(1) * num_block == vec.size(0) && vec.size(1) == 1);
     int64_t num_substride = subblock.size(0);
     assert((num_substride % 16) == 0);
     auto ret = torch::zeros_like(vec, torch::kFloat32);
-    float* pa = (float*)subblock.data_ptr(), *cur_pa = pa;
-    float* pb = (float*)vec.data_ptr(), *cur_pb = pb;
-    float* pc = (float*)ret.data_ptr(), *cur_pc = pc;
+    float* pa = (float*)subblock.data_ptr();
+    float* pb = (float*)vec.data_ptr();
+    float* pc = (float*)ret.data_ptr();
     for (int64_t i = 0; i < num_block; ++i) {
         // Print progress (optional)
         for (int64_t j = 0; j < num_substride; ++j) {
@@ -44,6 +46,34 @@ torch::Tensor hessianorpinv_matmul_vector_avx2(const torch::Tensor& subblock, co
     return ret;
 }
 
+// 必须确保使用的tensor都是连续的
+torch::Tensor hessianorpinv_matmul_vector_v1(const py::list& py_list, const torch::Tensor& vec) {
+  vector<torch::Tensor> tensors;
+  for (auto item : py_list) {
+	  tensors.push_back(py::cast<torch::Tensor>(item));
+  }
+  auto ret = torch::zeros_like(vec, torch::kFloat32);
+  float* pc = (float*)ret.data_ptr();
+  int num_block = tensors.size();
+  int row_base = 0;
+  float* pb = (float*)vec.data_ptr();
+  int num_substride = 0;
+  for (int64_t i = 0; i < num_block; ++i, row_base += num_substride) {
+	  auto& tensor = tensors.at(i);
+	  num_substride = tensor.size(0);
+	  assert(tensor.dim() == 2 && tensor.size(1) == num_substride);
+  	  float* pa = (float*)tensor.data_ptr();
+	  for (int64_t j = 0; j < num_substride; ++j) {
+		  for (int k = 0; k < num_substride; ++k) {
+			  pc[row_base + j] += pa[j * num_substride + k] * pb[row_base + k];
+		  }
+	  }
+  }
+  assert(vec.dim() == 2 && row_base == vec.size(0));
+  return ret;
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-        m.def("hessianorpinv_matmul_vector", &hessianorpinv_matmul_vector_avx2, "Custom implementation of sparse diagonal hessian matrix(or its pseduo-inverse matrix) matmul a vector");
+	m.def("hessianorpinv_matmul_vector", &hessianorpinv_matmul_vector_avx2, "Custom implementation of sparse diagonal hessian matrix(or its pseduo-inverse matrix) matmul a vector");
+	m.def("hessianorpinv_matmul_vector_v1", &hessianorpinv_matmul_vector_v1, "Custom implementation of sparse diagonal hessian matrix(or its pseduo-inverse matrix) matmul a vector");
 }
